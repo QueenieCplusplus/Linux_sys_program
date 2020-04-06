5f2eec3102c6a281d6b3b9a569b73d7fd5e5b074
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-04-06 20:33:47 +0800

    Add files via upload

---
+++ open.c
@@ -0,0 +1,17 @@
+//2020.4.06.pm8:20, by Vivy Chen (Queen)
+//FileIO
+//open()
+// int open (const char *name, int flags);
+// int open (const char *name, int flags, mode_t mode);
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int fd = open("~/pintred/Desktop/MBTI/ＩＮＦＪ/INFJ.png",O_RDONLY);
+
+if(fd == -1) {
+    print("error occurs.");
+}
+
+
