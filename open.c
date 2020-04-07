a58f23df23cec9a75625e577ece2d90ac12996f4
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-04-07 20:05:37 +0800

    Add files via upload

--- open.c
+++ open.c
@@ -4,14 +4,33 @@
 // int open (const char *name, int flags);
 // int open (const char *name, int flags, mode_t mode);
 
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int fd = open("~/pintred/Desktop/MBTI/ＩＮＦＪ/INFJ.png",O_RDONLY);
+int funcQ(){
 
-if(fd == -1) {
-    printf("error occurs.");
+    int fd = open("/Users/pintred/Desktop/bus_schedule.png ",O_RDONLY);
+
+    if(fd == -1) {
+        printf("error occurs.");
+    }
 }
 
+int main(){
 
+    int result;
+    result = funcQ();
+    printf("%d", result);
+    return result;
+
+}
+
+// [Running] cd "/Users/pintred/Desktop/" && gcc open.c -o open && "/Users/pintred/Desktop/"open
+// open.c:20:1: warning: control reaches end of non-void function [-Wreturn-type]
+// }
+// ^
+// 1 warning generated.
+// error occurs.0
+// [Done] exited with code=0 in 0.428 seconds
